dbaf8cb528f8c3ab5b37c56a88143a0aaf1ba299
kalpitkansara <kalpit kansara>
2025-07-25 17:20:59 +0530

    backend

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello via Bun!");
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req,res) => {
+        res.json({
+            success:true,
+            massage:"welcome to note_wave",
+        })
+});
+
+app.post("/keepThings",(req,res) =>{
+        const {title,content,time} = req.body;
+        res.json({
+            success:true,
+            massage:"come into keepThings",
+            title,
+            content,
+            time,
+        })
+});
+
+app.get("/note",(req,res) => {
+        res.json({
+            success:true,
+            massage:"namste to note_wave",
+        })
+});
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+});
